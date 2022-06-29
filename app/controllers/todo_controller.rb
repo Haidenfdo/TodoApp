52c305f391667a717191fa6704f59a7d213cd65d
Haiden Arulappan <you@example.com>
2022-06-29 11:50:46 +0530

    Add

---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,36 @@
+class TodoController < ApplicationController
+
+    def index
+       @todos_list = Todo.all.map do |use|
+        use
+    end
+    @todos_list
+  end
+        def create
+            todo=Todo.new(user_params)
+            todo.save
+            redirect_to "/"
+          end
+        
+          def delete
+            id = params[:del_id]
+            del_todo= Todo.find(id)
+            del_todo.destroy
+            del_todo.save
+            redirect_to "/"
+           end
+         
+           def update
+              id=params[:id]
+              todo_upt=Todo.find(id)
+              todo_upt.status :True
+              todo_upt.save
+              redirect_to "/"
+           end
+    private
+          def user_params
+          params.require(:todo).permit(:todo_text,:due_date,:status)
+     end
+
+
+    end
